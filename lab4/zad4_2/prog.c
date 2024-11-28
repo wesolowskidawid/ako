ddcefaeb48951eaaca20f7cfc7b3f0be4cb94ac0
Dawid Wesołowski <123513502+wesolowskidawid@users.noreply.github.com>
2024-11-28 14:50:54 +0100

    zad4_2

---
+++ lab4/zad4_2/prog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void liczba_przeciwna(int* a);
+
+int main()
+{
+	int m;
+	m = -5;
+	liczba_przeciwna(&m);
+	printf("\n m = %d\n", m);
+	m = 7;
+	liczba_przeciwna(&m);
+	printf("\n m = %d\n", m);
+	return 0;
+}
---
+++ lab4/zad4_2/program.asm
@@ -0,0 +1,21 @@
+.686
+.model flat
+
+public _liczba_przeciwna
+
+.code
+	_liczba_przeciwna PROC
+		push ebp ; zapisanie zawartoœci EBP na stosie
+		mov ebp,esp ; kopiowanie zawartoœci ESP do EBP
+		push ebx ; przechowanie zawartoœci rejestru EBX
+		; wpisanie do rejestru EBX adresu zmiennej zdefiniowanej
+		; w kodzie w jêzyku C
+		mov ebx, [ebp+8]
+		mov eax, [ebx] ; odczytanie wartoœci zmiennej
+		neg eax
+		mov [ebx], eax ; odes³anie wyniku do zmiennej
+		pop ebx
+		pop ebp
+		ret
+	_liczba_przeciwna ENDP
+ END
